0f1d4da1a0a54219997637e5970e125eefe6beba
jonah-cal <jonahmt@berkeley.edu>
2022-10-11 16:44:20 -0700

    Began project. So far, have started Main class to parse input.

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+/**
+ * Main class for Huffman Coding application.
+ *
+ * @author Jonah Tharakan
+ */
+public class Main {
+
+    /**
+     * Parses command arguments and calls the appropriate operation.
+     * ARGS is the array of inputs.
+     *
+     * Possible commands:
+     * "help"
+     * "command" "file name"
+     */
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            invalidInput(args);
+        }
+
+        // Switch on command
+        switch (args[0]) {
+            case "encode":
+                encode(args);
+        }
+
+    }
+
+    /**
+     * The command for compressing a plaintext file into binary via Huffman encoding.
+     * Method first validates that the file exists. Then,
+     * method creates an Encoder object which will handle the process.
+     */
+    private static void encode(String[] args) {
+
+    }
+
+    private static void invalidInput(String[] args) {
+        System.out.println("Your input command of ");
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            s.append(args[i]).append(" ");
+        }
+        if (args.length == 0) {
+            System.out.println("- BLANK -");
+        } else {
+            System.out.println(s.toString());
+        }
+        System.out.println("is invalid. Please run with command \"help\" for a list/description \nof possible commands.");
+    }
+
+}
